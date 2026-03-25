49fd19728bf84cf15b86b189f1cbf73cb6d94d1e
hvngocthanh-CS-UIT <huynhvongocthanh@gmail.com>
2026-03-25 15:02:39 +0700

    fix_config_to_server

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_origins=["http://localhost:3000", "http://localhost:5173", "https://mathquest.aipower.vn"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
